040fd186c0e31ae705abef665890410fd07899ed
mahdiarn <mahdiarnaufal@gmail.com>
2018-03-27 17:29:06 -0700

    test

--- tes3.py
+++ tes3.py
@@ -6,21 +6,23 @@ from math import *
 
 vertices = (
 	(0, 1, 1),
-	(3, 1, 1),
+	(2, 1, 1),
 	(3, 2, 1),
 	(5, 2, 1),
-	(5, 1, 1),
+	(6, 1, 1),
 	(7, 1, 1),
-	(7, 2, 1),
-	(9, 2, 1),
-	(9, 1, 1),
+	(8, 2, 1),
+	(10, 2, 1),
 	(11, 1, 1),
+	(12, 1, 1),
+	(12, 3, 1),
 	(11, 4, 1),
 	(10, 4, 1),
-	(10, 7, 1),
-	(3, 7, 1), 
+	(9, 5, 1),
+	(4, 5, 1),
 	(3, 4, 1),
-	(0, 4, 1))
+	(1, 4, 1),
+	(0, 3, 1),)
 edges = (
 	(0, 1),
 	(1, 2),
@@ -37,46 +39,86 @@ edges = (
 	(12, 13),
 	(13, 14),
 	(14, 15),
-	(0, 15)
+	(15, 16),
+	(16,17),
+	(17,0),
 	)
+
+verticesColor = (
+(0, 1, 1),
+(2, 1, 1),
+(2, 3, 1),
+(0, 3, 1),
+(3, 4, 1),
+(1, 4, 1),
+(11, 3, 1),
+(9, 5, 1),
+(4, 5, 1),
+(3, 3, 1),
+(3, 2, 1),
+(10, 3, 1),
+(10, 2, 1),
+(11, 1, 1),
+(12, 1, 1),
+(12, 3, 1),
+(11, 4, 1),
+(10, 4, 1),
+(5, 2, 1),
+(6, 1, 1),
+(7, 1, 1),
+(8, 2, 1),
+)
 surfaces = (
-	(0, 1, 14, 15),
-	(2, 7, 12, 13),
-	(8, 9, 10, 11),
-	(3, 4, 5, 6))
+	(0, 1, 2, 3),
+	(2, 4, 5, 3),
+	(2, 6, 7, 8),
+	(2, 9, 10, 1),
+	(9, 11, 12, 10),
+	(11, 6, 13, 12),
+	(6, 13, 14, 15),
+	(6, 15, 16, 17),
+	(18, 19, 20, 21),
+	)
 
-def CarBody(time):
+def drawCircle(time,radius,sides,posx,posy):
 
-	glBegin(GL_LINES)
-	for edge in edges:
-		for vertex in edge:
-			glVertex3fv(vertices[vertex])
+	glBegin(GL_POLYGON)
+	for i in range(100):
+		if ((i + time) % 4 >= 2) :
+			cosine = radius * cos (i*2*pi/sides) + posx
+			sine = radius * sin(i*2*pi/sides) + posy
+			glVertex2f(cosine, sine)
 	glEnd()
+
+def drawCarBody():
+	global surfaces
+	global verticesColor
 	glBegin(GL_QUADS)
 	for surface in surfaces:
 		for vertex in surface:
+			glColor3fv((0, 0, 1))
+			glVertex3fv(verticesColor[vertex])
+	glEnd()
+
+def drawCarOutline():
+	global surfaces
+	global verticesColor
+	glBegin(GL_LINES)
+	for edge in edges:
+		for vertex in edge:
 			glColor3fv((1, 0, 0))
 			glVertex3fv(vertices[vertex])
 	glEnd()
-	radius = 0.9
-	sides = 32
-	posx, posy = 3.9,1
-	glBegin(GL_POLYGON)
-	for i in range(100):
-		if ((i + time) % 4 >= 2) :
-			cosine = radius * cos (i*2*pi/sides) + posx
-			sine = radius * sin(i*2*pi/sides) + posy
-			glVertex2f(cosine, sine)
-	glEnd()
 
-	posx, posy = 8.2,1
-	glBegin(GL_POLYGON)
-	for i in range(100):
-		if ((i + time) % 4 >= 2) :
-			cosine = radius * cos (i*2*pi/sides) + posx
-			sine = radius * sin(i*2*pi/sides) + posy
-			glVertex2f(cosine, sine)
-	glEnd()
+def CarBody(time):
+
+	drawCarBody()
+	drawCarOutline()
+	glColor3fv((1, 1, 1))
+	radius,sides,posx, posy = 0.9,32,4,0.8
+	drawCircle(time,radius,sides,posx,posy)
+	posx, posy = 9.2,0.8
+	drawCircle(time,radius,sides,posx,posy)
 
 def main():
 	pygame.init()
